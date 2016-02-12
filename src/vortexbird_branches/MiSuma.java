8d212055a01d7fcc74d6a279af9b71c761591868
Juan Camilo <jpuente@vortexbird.com>
2016-02-12 17:41:20 -0500

    CAMILO

--- src/vortexbird_branches/MiSuma.java
+++ src/vortexbird_branches/MiSuma.java
@@ -19,11 +19,18 @@ public class MiSuma {
 		
 		
 
+		System.out.println(1 + 2);
+		System.out.println("Mi primer commit");
+		// otr comentrario frank!!!
+		
 	}
 	
 	
 	public Long multiplicacionMolecular(Long test, Long test2) {
 		//juan camilo 
+		
+		// Frank Edward Daza estuvo aquí!!!!!
+		//  123412341234
 		return 0l;
 	}
 
