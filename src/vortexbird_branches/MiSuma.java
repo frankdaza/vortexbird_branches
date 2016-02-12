22e0338ccf368138fc585d82626e5ccf60a829b5
Andres Felipe Vargas Lopez <avargas@vortexbird.com>
2016-02-12 18:03:44 -0500

    AFVL

--- src/vortexbird_branches/MiSuma.java
+++ src/vortexbird_branches/MiSuma.java
@@ -14,22 +14,7 @@ public class MiSuma {
 		 * 
 		 * 
 		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
+		LHFKGCKJ YIJVKYGCOVYBIUFGY
 		 * 
 		 * 
 		 */
@@ -62,29 +47,6 @@ public class MiSuma {
 		
 		// Frank Edward Daza estuvo aquí!!!!!
 		//  123412341234
-	public Long multiplicacionMolecular222(Long test, Long test2) {
-		// NADIE ESTUVO AQUÍ"""""
-		// Frank Edward Daza estuvo aquí!!!!!
-		//  123412341234
-		return 0l;
-	}
-	
-	public Long multiplicacionMoCHANGO MAN22(Long test, Long test2) {
-		
-		// Frank Edward Daza estuvo aquí!!!!!
-		//  123412341234
-	public Long multiplicacionMolecular222(Long test, Long test2) {
-		// NADIE ESTUVO AQUÍ"""""
-		// Frank Edward Daza estuvo aquí!!!!!
-		//  123412341234
-		return 0l;
-	}
-	
-	public Long multiplicacionMoCHANGO MAN22(Long test, Long test2) {
-		
-		// Frank Edward Daza estuvo aquí!!!!!
-		//  123412341234
-		return 0l;
-	}
+	}	
 
 }
