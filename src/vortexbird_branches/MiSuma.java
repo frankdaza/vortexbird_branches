86acff2c60aea4b06158fb08d55dca8036c38544
Frank Edward Daza González <frank.daza2@gmail.com>
2016-02-12 17:29:27 -0500

    conflictos  a la orden

--- src/vortexbird_branches/MiSuma.java
+++ src/vortexbird_branches/MiSuma.java
@@ -4,8 +4,10 @@ public class MiSuma {
 
 	public static void main(String[] args) {
 		
+		System.out.println(1 + 2);
 		System.out.println("Mi primer commit");
-
+		// otr comentrario frank!!!
+		
 	}
 
 }
