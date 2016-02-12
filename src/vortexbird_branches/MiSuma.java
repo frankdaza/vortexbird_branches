4e5fbaa670117f1482a5944a32c04087e3d72ed0
Cesar Lopez <clopez@vortexbird.com>
2016-02-12 17:47:06 -0500

    comit sol conf

--- src/vortexbird_branches/MiSuma.java
+++ src/vortexbird_branches/MiSuma.java
@@ -4,14 +4,47 @@ public class MiSuma {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Camilo es mucho más gay");
-
-	
+		System.out.println(1 + 2);
+		System.out.println("Mi primer commit");
+		// otr comen
+		System.out.println(1 + 2);
+		System.out.println("Mi primer commit");
+		// otr comen
+		
+		
+		System.out.println(1 + 2);
+		System.out.println("Mi primer commit");
+		// otr comen
+		
+		System.out.println(1 + 2);
+		System.out.println("Mi primer commit");
+		// otr comen
+		
+		sd
+		fa
+		sdf
+		asd
+		f
+		asdf
+		as
+		df
+		asdftrario frank!!!
+		asdfasdfasdf
+		
 	}
 	
 	
-	public Long multiplicacionMolecular(Long test, Long test2) {
-		//juan camilo
+	public Long multiplicacionMolecular222(Long test, Long test2) {
+		// NADIE ESTUVO AQUÍ"""""
+		// Frank Edward Daza estuvo aquí!!!!!
+		//  123412341234
+		return 0l;
+	}
+	
+	public Long multiplicacionMoCHANGO MAN22(Long test, Long test2) {
+		
+		// Frank Edward Daza estuvo aquí!!!!!
+		//  123412341234
 		return 0l;
 	}
 
