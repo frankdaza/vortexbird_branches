95fb89794b20b0613317457431a8b7f6fa21d124
Frank Edward Daza González <frank.daza2@gmail.com>
2016-02-12 17:44:32 -0500

    CAMBIO EN LA NUBE POR FRANK!!

--- src/vortexbird_branches/MiSuma.java
+++ src/vortexbird_branches/MiSuma.java
@@ -11,8 +11,14 @@ public class MiSuma {
 	}
 	
 	
-	public Long multiplicacionMolecular(Long test, Long test2) {
-		//juan camilo 
+	public Long multiplicacionMolecular222(Long test, Long test2) {
+		// NADIE ESTUVO AQUÍ"""""
+		// Frank Edward Daza estuvo aquí!!!!!
+		//  123412341234
+		return 0l;
+	}
+	
+	public Long multiplicacionMoCHANGO MAN22(Long test, Long test2) {
 		
 		// Frank Edward Daza estuvo aquí!!!!!
 		//  123412341234
