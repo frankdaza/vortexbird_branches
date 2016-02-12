24e4c57070bcd8fb9106404cb22a3b8b7f8abd7f
Juan Camilo <jpuente@vortexbird.com>
2016-02-12 17:34:55 -0500

    CAMILO

--- src/vortexbird_branches/MiSuma.java
+++ src/vortexbird_branches/MiSuma.java
@@ -4,13 +4,26 @@ public class MiSuma {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Camilo es gay");
+		System.out.println("Camilo esrftyuiop gay");
+		System.out.println("Camilo esrftyuiop gay");
+		System.out.println("Camilo esrftyuiop gay");
+		System.out.println("Camilo esrftyuiop gay");
+		System.out.println("Camilo esrftyuiop gay");System.out.println("Camilo esrftyuiop gay");
+		
+		System.out.println("Camilo esrftyuiop gay");
+		System.out.println("Camilo esrftyuiop gay");
+		System.out.println("Camilo esrftyuiop gay");
+		System.out.println("Camilo esrftyuiop gay");
+		
+		System.out.println("CESAR ES HIPER MEGA ULTRA ARCHI RE CONTRA MEGA PLUS FRUTIFANTASTICAMENTE GAY");
+		
+		
 
 	}
 	
 	
 	public Long multiplicacionMolecular(Long test, Long test2) {
-		//juan camilo
+		//juan camilo 
 		return 0l;
 	}
 
